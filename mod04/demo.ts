1dec68a1f0e0045687f8cc902702db834ec97e55
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-02-20 17:12:20 +0000

    demo

--- mod04/demo.ts
+++ mod04/demo.ts
@@ -1,10 +1,14 @@
+
 document.getElementById('add')
     .addEventListener('click', onClick);
-function onClick() {
-    var el = document.getElementById('x');
+
+function onClick(){
+    var el = <HTMLInputElement>document.getElementById('x');
     var x = +el.value;
+
     add(x, x);
 }
-function add(x, y) {
+
+function add(x: number, y: number){
     console.log(x + y);
 }
